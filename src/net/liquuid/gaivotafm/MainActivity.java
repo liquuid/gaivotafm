82b39fe9406ddc7dc6045a134518cc4e6d7a9a21
Fernando Silva <liquuid@liquuid.net>
2014-06-13 13:23:37 -0300

    new release

--- src/net/liquuid/gaivotafm/MainActivity.java
+++ src/net/liquuid/gaivotafm/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends Activity implements OnClickListener {
 	private Button btStart;
 	private WebView webView;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
