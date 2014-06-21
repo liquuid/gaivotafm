c0df406c9552eb0f89cef959219ae58990b9b0ff
Fernando Silva <liquuid@liquuid.net>
2014-06-21 02:26:51 -0300

    possibilidade de escolha da qualidade do stream

--- src/net/liquuid/gaivotafm/MainActivity.java
+++ src/net/liquuid/gaivotafm/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -16,8 +18,11 @@ public class MainActivity extends Activity implements OnClickListener {
 	private PlayerMp3 player = new PlayerMp3();
 	private ImageButton btStart;
 	private ToggleButton btStatus;
+	private RadioGroup radiogroup;
 	private WebView webView;
-	private String mp3 = "http://stream.gaivota.fm.br:8000/1049.mp3";
+	private String mp3baixa = "http://stream.gaivota.fm.br:8000/1049.mp3";
+	private String mp3alta = "http://stream.gaivota.fm.br:8000/1049-alta.mp3";
+	private String mp3 = mp3alta;
 	private String RSSUrl = "http://gaivota.fm.br/blog/feed";
 	private XmlHandler obj;
 	private StatusCheck stsCheck = new StatusCheck();
@@ -29,48 +34,60 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		btStart = (ImageButton) findViewById(R.id.button1);
 		btStatus = (ToggleButton) findViewById(R.id.toggleButton1);
-		stsCheck.check(mp3, btStatus);
+		radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+		radiogroup.check(R.id.altaQualidade);
 
 		if (savedInstanceState != null) {
 			String status = savedInstanceState.getString("playerStatus");
+			int rg = savedInstanceState.getInt("radioGroup");
+			if (rg == 1) {
+				mp3 = mp3alta;
+			} else if (rg == 2) {
+				mp3 = mp3baixa;
+			}
+
 			if (status == "playing") {
-				btStart.setImageResource(R.drawable.pause64); //setText("Pause");
+				btStart.setImageResource(R.drawable.pause64); // setText("Pause");
 				player.start(mp3);
 			} else {
 				btStart.setImageResource(R.drawable.play64);
 				player.pause();
 			}
 		}
+
+		stsCheck.check(mp3, btStatus);
+
 		obj = new XmlHandler(RSSUrl);
 		obj.fetchXML();
 		obj.getPosts();
 
-		while (obj.parsingComplete);
+		while (obj.parsingComplete)
+			;
 		System.out.println(obj.getPosts());
-		//html = obj.getPosts();
-		html = "<html><head><style type=\"text/css\">hr { clear: both; float: none;" +
-				" width: 100%; height: 1px; margin: 1.0em 0; border: none; " +
-				"background: #ddd; background-image: -webkit-gradient( linear, " +
-				"left bottom, right bottom,color-stop(0, rgb(255,255,255)),color-stop" +
-				"(0.1, rgb(221,221,221)),color-stop(0.9, rgb(221,221,221)),color-stop" +
-				"(1, rgb(255,255,255)));}</style></head><body>" +
-				"<a href=\"http://gaivota.fm.br/blog/novidades-no-ar\">Novidades no ar</a>" +
-				"<br><hr> <a href=\"http://gaivota.fm.br/blog/recado-de-um-ouvinte\">" +
-				"Recado de um ouvinte</a><br><hr> <a href=\"http://gaivota.fm.br/blog\">" +
-				"Blogs Gaivota FM</a><br><hr> <a href=\"http://gaivota.fm.br/blog/amplific" +
-				"ador-marshall-valvstate-vs65r\">Amplificador Marshall Valvstate VS65R</a>" +
-				"<br><hr> <a href=\"http://gaivota.fm.br/blog/amplificador-fender-frontman-25r-1\">" +
-				"Amplificador Fender Frontman 25R</a><br><hr> <a href=\"http://gaivota.fm.br/b" +
-				"log/pedal-boss-ps-3\">Pedal Boss PS-3</a><br><hr> <a href=\"http://gaivota.fm" +
-				".br/blog/pronatec-curso-operador-de-udio\">Pronatec - Curso Operador de &Aacute;udio" +
-				"</a><br><hr> <a href=\"http://gaivota.fm.br/blog/reformula-o-do-portal-gaivota-fm\">" +
-				"Reformula&ccedil;&atilde;o do portal Gaivota FM</a><br><hr> </body></html>";
+		// html = obj.getPosts();
+		html = "<html><head><style type=\"text/css\">hr { clear: both; float: none;"
+				+ " width: 100%; height: 1px; margin: 1.0em 0; border: none; "
+				+ "background: #ddd; background-image: -webkit-gradient( linear, "
+				+ "left bottom, right bottom,color-stop(0, rgb(255,255,255)),color-stop"
+				+ "(0.1, rgb(221,221,221)),color-stop(0.9, rgb(221,221,221)),color-stop"
+				+ "(1, rgb(255,255,255)));}</style></head><body>"
+				+ "<a href=\"http://gaivota.fm.br/blog/novidades-no-ar\">Novidades no ar</a>"
+				+ "<br><hr> <a href=\"http://gaivota.fm.br/blog/recado-de-um-ouvinte\">"
+				+ "Recado de um ouvinte</a><br><hr> <a href=\"http://gaivota.fm.br/blog\">"
+				+ "Blogs Gaivota FM</a><br><hr> <a href=\"http://gaivota.fm.br/blog/amplific"
+				+ "ador-marshall-valvstate-vs65r\">Amplificador Marshall Valvstate VS65R</a>"
+				+ "<br><hr> <a href=\"http://gaivota.fm.br/blog/amplificador-fender-frontman-25r-1\">"
+				+ "Amplificador Fender Frontman 25R</a><br><hr> <a href=\"http://gaivota.fm.br/b"
+				+ "log/pedal-boss-ps-3\">Pedal Boss PS-3</a><br><hr> <a href=\"http://gaivota.fm"
+				+ ".br/blog/pronatec-curso-operador-de-udio\">Pronatec - Curso Operador de &Aacute;udio"
+				+ "</a><br><hr> <a href=\"http://gaivota.fm.br/blog/reformula-o-do-portal-gaivota-fm\">"
+				+ "Reformula&ccedil;&atilde;o do portal Gaivota FM</a><br><hr> </body></html>";
 
-		
 		btStart.setOnClickListener(this);
 
 		webView = (WebView) findViewById(R.id.webView1);
-		
+
 		webView.loadData(html, "text/html", "utf-8");
 		// webView.loadUrl("http://gaivota.fm.br/blog/feed");
 
@@ -81,7 +98,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	 * menu; this adds items to the action bar if it is present.
 	 * getMenuInflater().inflate(R.menu.main, menu); return true; }
 	 */
- 
+
 	@Override
 	public void onClick(View view) {
 		try {
@@ -90,6 +107,12 @@ public class MainActivity extends Activity implements OnClickListener {
 					btStart.setImageResource(R.drawable.play64);
 					player.pause();
 				} else {
+					int rg = radiogroup.getCheckedRadioButtonId();
+					if (rg == 1) {
+						mp3 = mp3alta;
+					} else if (rg == 2) {
+						mp3 = mp3baixa;
+					}
 					player.start(mp3);
 					btStart.setImageResource(R.drawable.pause64);
 				}
@@ -100,10 +123,33 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 	}
 
+	public void onRadioButtonClicked(View view) {
+		boolean checked = ((RadioButton) view).isChecked();
+
+		switch (view.getId()) {
+		case R.id.altaQualidade:
+			if (checked) {
+				mp3 = mp3alta;
+				if (player.isPlaying())
+					player.start(mp3);
+
+			}
+			break;
+		case R.id.baixaQualidade:
+			if (checked){
+				mp3 = mp3baixa;
+				if (player.isPlaying())
+					player.start(mp3);
+			}
+			break;
+		}
+	}
+
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putString("playerStatus", player.status());
+		outState.putInt("radioGroup", radiogroup.getCheckedRadioButtonId());
 	}
 
 	@Override
@@ -111,7 +157,5 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onDestroy();
 		player.fechar();
 	}
-
-	
 
 }
